d75a9551c9217c3e7de966538ac095f55bab8226
Jung gyun Ahn <Junggyun.ahn@navercorp.com>
2023-08-29 17:16:06 +0900

    apply review comment

--- Sources/Document/Document.swift
+++ Sources/Document/Document.swift
@@ -500,18 +500,12 @@ public actor Document {
                 var isMine = false
                 var isOthers = false
 
-                if let event = event as? WatchedEvent {
-                    if event.value.clientID == id {
-                        isMine = true
-                    } else {
-                        isOthers = true
-                    }
-                } else if let event = event as? UnwatchedEvent {
-                    if event.value == id {
-                        isMine = true
-                    } else {
-                        isOthers = true
-                    }
+                if event is InitializedEvent {
+                    isMine = true
+                } else if event is WatchedEvent {
+                    isOthers = true
+                } else if event is UnwatchedEvent {
+                    isOthers = true
                 } else if let event = event as? PresenceChangedEvent {
                     if event.value.clientID == id {
                         isMine = true
