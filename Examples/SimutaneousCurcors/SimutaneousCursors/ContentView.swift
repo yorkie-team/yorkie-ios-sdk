70b99b4bb03c4ca04ae4cdcca8b186366d6fe6c6
Ivan <18699881+donhatkha@users.noreply.github.com>
2025-10-09 09:54:29 +0700

    Add Example Simutaneous cursors (#228)
    * Add sample Todo MVC
    
    * Add POC Simutaneous Cursors
    
    * Add swiftFormat
    
    * Lack of convert from JS to CRDTArray and CRDTObject in Converter
    
    * Add Anonymous for FE User

---
+++ Examples/SimutaneousCurcors/SimutaneousCursors/ContentView.swift
@@ -0,0 +1,171 @@
+/*
+ * Copyright 2025 The Yorkie Authors. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import SwiftUI
+
+struct ContentView: View {
+    let name: String
+    @State var viewModel = ContentViewModel()
+    @State private var dragOffset: CGSize = .zero
+    @State var currentPosition = CGPoint(x: 0, y: 0)
+
+    init(name: String) {
+        self.name = name
+    }
+
+    var body: some View {
+        ZStack {
+            Color.white.opacity(0.1)
+                .onTapGesture { location in
+                    self.changePosition(location)
+                    self.currentPosition = location
+                }
+                .gesture(self.dragGesture)
+
+            self.canvasView
+            self.menuView
+
+            VStack {
+                Text(self.name)
+                    .foregroundStyle(Color.white)
+                    .padding(3)
+                    .background(Color.red)
+                    .cornerRadius(2)
+                Image(systemName: self.viewModel.currentCursor.systemImageName)
+            }
+            .position(self.currentPosition)
+        }
+        .ignoresSafeArea()
+        .task {
+            await self.viewModel.initializeClient(with: self.name)
+        }
+        .onDisappear {
+            Task {
+                await self.viewModel.deactivate()
+            }
+        }
+    }
+
+    private var loadingView: some View {
+        ProgressView()
+    }
+
+    private func errorView(_ error: TDError) -> some View {
+        Text("Error occur: \(error.localizedDescription)")
+    }
+
+    var canvasView: some View {
+        VStack {
+            ForEach(self.viewModel.uiPresenecs) { peer in
+                VStack {
+                    Text(peer.presence.name)
+                        .foregroundStyle(Color.white)
+                        .padding(3)
+                        .background(Color.red)
+                        .cornerRadius(2)
+                    Image(systemName: peer.presence.cursorShape.systemImageName)
+                        .resizable()
+                        .scaledToFit()
+                        .foregroundStyle(Color.red)
+                        .frame(width: 20, height: 20)
+                }
+                .position(
+                    x: peer.presence.cursor.xPos,
+                    y: peer.presence.cursor.yPos
+                )
+            }
+        }
+    }
+
+    var dragGesture: some Gesture {
+        DragGesture()
+            .onChanged { value in
+                self.dragOffset = value.translation
+                self.currentPosition = value.location
+                self.changePosition(self.currentPosition)
+            }
+            .onEnded { _ in
+                withAnimation(.bouncy) {
+                    self.dragOffset = .zero
+                }
+            }
+    }
+
+    var menuView: some View {
+        VStack {
+            Spacer()
+
+            HStack {
+                Group {
+                    Button {
+                        self.viewModel.currentCursor = .heart
+                    } label: {
+                        Image(systemName: CursorShape.heart.systemImageName)
+                            .resizable()
+                            .scaledToFit()
+                            .foregroundStyle(Color.red)
+                            .background(Color.white)
+                    }
+                    Button {
+                        self.viewModel.currentCursor = .thumbs
+                    } label: {
+                        Image(systemName: CursorShape.thumbs.systemImageName)
+                            .resizable()
+                            .scaledToFit()
+                            .foregroundStyle(Color.yellow)
+                            .background(Color.white)
+                    }
+
+                    Button {
+                        self.viewModel.currentCursor = .pen
+                    } label: {
+                        Image(systemName: CursorShape.pen.systemImageName)
+                            .resizable()
+                            .scaledToFit()
+                            .foregroundStyle(Color.black)
+                            .background(Color.white)
+                    }
+
+                    Button {
+                        self.viewModel.currentCursor = .cursor
+                    } label: {
+                        Image(systemName: CursorShape.cursor.systemImageName)
+                            .resizable()
+                            .scaledToFit()
+                            .foregroundStyle(Color.black)
+                            .background(Color.white)
+                    }
+                }
+                .frame(width: 28, height: 28)
+                .padding(12)
+                .background(
+                    RoundedRectangle(cornerRadius: 12)
+                        .stroke(Color.red, lineWidth: 2)
+                )
+            }
+
+            Text("\(self.viewModel.uiPresenecs.count + 1) users here!")
+        }
+    }
+
+    func changePosition(_ position: CGPoint) {
+        self.viewModel.updatePosition(position)
+    }
+}
+
+#Preview {
+    ContentView(name: "iOS")
+}
---
+++ Examples/SimutaneousCurcors/SimutaneousCursors/ContentViewModel.swift
@@ -0,0 +1,149 @@
+/*
+ * Copyright 2025 The Yorkie Authors. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import Combine
+import Foundation
+import SwiftUI
+import Yorkie
+
+@Observable
+class ContentViewModel {
+    let width = UIScreen.current!.bounds.size.width
+    let height = UIScreen.current!.bounds.size.height
+
+    @ObservationIgnored private var client: Client
+    @ObservationIgnored private let document: Document
+    @ObservationIgnored var presenecs = [Presence]()
+
+    var uiPresenecs = [Model]()
+    var currentCursor: CursorShape = .heart {
+        didSet {
+            self.updateCursor()
+        }
+    }
+
+    private(set) var state = ContentState.loading
+
+    init() {
+        self.client = Client(Constant.serverAddress)
+        self.document = Document(key: Constant.documentKey)
+    }
+
+    func initializeClient(with name: String) async {
+        self.state = .loading
+        do {
+            try await self.client.activate()
+
+            let doc = try await client.attach(self.document, [
+                "name": name,
+                "cursorShape": "heart",
+                "cursor": ["xPos": 0.5, "yPos": 0.5],
+                "pointerDown": "false"
+            ])
+            try self.document.update { root, _ in
+                var text = root.content as? JSONText
+                if text == nil {
+                    root.content = JSONText()
+                    text = root.content as? JSONText
+                }
+            }
+            let pres = doc.getPresences()
+            self.mapFromPrecenscToUI(pres)
+            self.state = .success
+
+            await self.watch()
+        } catch {
+            self.state = .error(.cannotInitClient("\(error.localizedDescription)"))
+        }
+    }
+
+    func deactivate() async {
+        do {
+            try await self.client.deactivate()
+        } catch {}
+    }
+
+    func watch() async {
+        self.document.subscribe { [weak self] event, document in
+            if case .syncStatusChanged = event.type {
+                let presences = document.getPresences()
+                self?.mapFromPrecenscToUI(presences)
+            } else if case .presenceChanged = event.type {
+                let presences = document.getPresences()
+                self?.mapFromPrecenscToUI(presences)
+            }
+        }
+    }
+
+    @MainActor
+    private func mapFromPrecenscToUI(_ peers: [PeerElement]) {
+        var _uiPresenecs = [Model]()
+
+        for peer in peers {
+            let id = peer.clientID
+            let presentModel = peer.presence
+            let name = presentModel["name"] as? String ?? "Anonymous"
+            let pointerDown = presentModel["pointerDown"] as? Int
+            let cursor = presentModel["cursor"] as? [String: Double]
+            let cursorShape = presentModel["cursorShape"] as? String
+
+            guard let pointerDown, let cursor, let cursorShape, let cursorShape = CursorShape(rawValue: cursorShape) else {
+                continue
+            }
+
+            guard let xPos = cursor["xPos"], let yPos = cursor["yPos"] else { fatalError() }
+            let realxPos = xPos * self.width
+            let realyPos = yPos * self.height
+
+            let model = Model(
+                clientID: id,
+                presence: .init(
+                    name: name,
+                    pointerDown: pointerDown,
+                    cursorShape: cursorShape,
+                    cursor: .init(xPos: realxPos, yPos: realyPos)
+                )
+            )
+
+            _uiPresenecs.append(model)
+        }
+
+        self.uiPresenecs.removeAll()
+        self.uiPresenecs = _uiPresenecs
+
+        print(self.uiPresenecs)
+    }
+
+    func updatePosition(_ position: CGPoint) {
+        do {
+            try self.document.update { _, presence in
+                presence.set(["cursor": ["xPos": position.x / self.width, "yPos": position.y / self.height]])
+            }
+        } catch {
+            self.state = .error(.cannotInitClient("\(error.localizedDescription)"))
+        }
+    }
+
+    func updateCursor() {
+        do {
+            try self.document.update { _, presence in
+                presence.set(["cursorShape": self.currentCursor])
+            }
+        } catch {
+            self.state = .error(.cannotInitClient("\(error.localizedDescription)"))
+        }
+    }
+}
